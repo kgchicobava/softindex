df04eeeb418ce229bdd75509aa6982dc718b7f70
kgchicobava <kgchicobava@gmail.com>
2020-02-06 01:15:06 +0200

    Updates

---
+++ precache-manifest.19247e16299911339926c15f307cb1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8150e79161a6f1c0a90c49ad24996cb9",
+    "url": "/softindex/index.html"
+  },
+  {
+    "revision": "c325d819bbcae17b017f",
+    "url": "/softindex/static/css/main.f2554af1.chunk.css"
+  },
+  {
+    "revision": "2453cba182f9039049f7",
+    "url": "/softindex/static/js/2.9d72729d.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/softindex/static/js/2.9d72729d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c325d819bbcae17b017f",
+    "url": "/softindex/static/js/main.4f5f6565.chunk.js"
+  },
+  {
+    "revision": "03df5840b98f719e4767",
+    "url": "/softindex/static/js/runtime-main.a9a42d05.js"
+  }
+]);
